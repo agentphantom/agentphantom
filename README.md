47ea86d57ab41b7a2e1a7f3324bcce78a8093c9c
agentphantom <phantom@phantom.im>
2021-11-05 16:15:22 -0600

    Refactored from feedback.

--- README.md
+++ README.md
@@ -4,23 +4,23 @@
 
 | Location | Mag | Date and Time (UTC) |
 |:---|:---|:---|
-| [113 km NNE of Lobujya, Nepal](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frkq) | 5 | 11-05-2021 01:48:31 |
-| [offshore Atacama, Chile](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frq2) | 4.9 | 11-05-2021 14:11:28 |
-| [99 km SE of Toba, Japan](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frlu) | 4.9 | 11-05-2021 04:55:56 |
-| [87 km SE of Severo-Kuril’sk, Russia](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frrf) | 4.8 | 11-05-2021 15:27:13 |
-| [34 km ENE of Bacolod, Philippines](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frlz) | 4.7 | 11-05-2021 05:05:17 |
-| [140 km ESE of Petropavlovsk-Kamchatsky, Russia](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frpi) | 4.6 | 11-05-2021 12:12:14 |
-| [south of the Fiji Islands](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frkj) | 4.5 | 11-05-2021 01:15:50 |
-| [34 km WNW of Salahonda, Colombia](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frle) | 4.5 | 11-05-2021 04:13:52 |
-| [166 km ESE of Petropavlovsk-Kamchatsky, Russia](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frpp) | 4.5 | 11-05-2021 12:44:38 |
-| [south of the Fiji Islands](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frnq) | 4.3 | 11-05-2021 10:05:51 |
-| [111 km WSW of La Libertad, Mexico](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frp3) | 4.3 | 11-05-2021 11:18:40 |
-| [69 km WNW of San Antonio de los Cobres, Argentina](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frku) | 4.2 | 11-05-2021 02:30:49 |
-| [195 km SE of Perryville, Alaska](https://earthquake.usgs.gov/earthquakes/eventpage/ak021e5oxsoi) | 3.9 | 11-04-2021 19:17:15 |
-| [49 km SE of Boca de Yuma, Dominican Republic](https://earthquake.usgs.gov/earthquakes/eventpage/pr2021309009) | 3.5 | 11-05-2021 13:19:26 |
-| [13km SW of Searles Valley, CA](https://earthquake.usgs.gov/earthquakes/eventpage/ci39849663) | 3.41 | 11-05-2021 12:03:13 |
-| [6 km WSW of Guánica, Puerto Rico](https://earthquake.usgs.gov/earthquakes/eventpage/pr2021309000) | 3.34 | 11-05-2021 01:42:18 |
-| [46 km WSW of Stella, Puerto Rico](https://earthquake.usgs.gov/earthquakes/eventpage/pr2021309007) | 3.3 | 11-05-2021 09:26:07 |
-| [212 km SE of Perryville, Alaska](https://earthquake.usgs.gov/earthquakes/eventpage/ak021e5rg938) | 3.3 | 11-04-2021 23:39:17 |
-| [4 km WSW of Guánica, Puerto Rico](https://earthquake.usgs.gov/earthquakes/eventpage/pr2021309006) | 3.19 | 11-05-2021 09:12:19 |
-| [11 km SSW of Guánica, Puerto Rico](https://earthquake.usgs.gov/earthquakes/eventpage/pr2021309004) | 3.18 | 11-05-2021 04:43:49 |
+| [113 km NNE of Lobujya, Nepal](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frkq) | 5 | 11-04-2021 19:48:31 |
+| [near the south coast of Honshu, Japan](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frlu) | 4.9 | 11-04-2021 22:55:56 |
+| [68 km W of Vallenar, Chile](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frq2) | 4.9 | 11-05-2021 08:11:28 |
+| [87 km SE of Severo-Kuril’sk, Russia](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frrf) | 4.8 | 11-05-2021 09:27:13 |
+| [34 km ENE of Bacolod, Philippines](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frlz) | 4.7 | 11-04-2021 23:05:17 |
+| [63 km SSE of Chimoio, Mozambique](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frur) | 4.6 | 11-05-2021 15:45:07 |
+| [16 km SSW of Plutarco Elías Calles (La Y Griega), Mexico](https://earthquake.usgs.gov/earthquakes/eventpage/us7000fru2) | 4.6 | 11-05-2021 14:18:26 |
+| [140 km ESE of Petropavlovsk-Kamchatsky, Russia](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frpi) | 4.6 | 11-05-2021 06:12:14 |
+| [south of the Fiji Islands](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frkj) | 4.5 | 11-04-2021 19:15:50 |
+| [34 km WNW of Salahonda, Colombia](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frle) | 4.5 | 11-04-2021 22:13:52 |
+| [166 km ESE of Petropavlovsk-Kamchatsky, Russia](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frpp) | 4.5 | 11-05-2021 06:44:38 |
+| [102 km SSE of Arica, Chile](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frth) | 4.5 | 11-05-2021 12:48:20 |
+| [south of the Fiji Islands](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frnq) | 4.3 | 11-05-2021 04:05:51 |
+| [111 km WSW of La Libertad, Mexico](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frp3) | 4.3 | 11-05-2021 05:18:40 |
+| [69 km WNW of San Antonio de los Cobres, Argentina](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frku) | 4.2 | 11-04-2021 20:30:49 |
+| [79 km ESE of Nago, Japan](https://earthquake.usgs.gov/earthquakes/eventpage/us7000frue) | 4.1 | 11-05-2021 14:56:42 |
+| [49 km SE of Boca de Yuma, Dominican Republic](https://earthquake.usgs.gov/earthquakes/eventpage/pr2021309009) | 3.5 | 11-05-2021 07:19:26 |
+| [14km SSW of Searles Valley, CA](https://earthquake.usgs.gov/earthquakes/eventpage/ci39849663) | 3.36 | 11-05-2021 06:03:13 |
+| [6 km WSW of Guánica, Puerto Rico](https://earthquake.usgs.gov/earthquakes/eventpage/pr2021309000) | 3.34 | 11-04-2021 19:42:18 |
+| [46 km WSW of Stella, Puerto Rico](https://earthquake.usgs.gov/earthquakes/eventpage/pr2021309007) | 3.3 | 11-05-2021 03:26:07 |
--- script.py
+++ script.py
@@ -10,84 +10,120 @@ URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson
 
 def main():
 
+    data = get_data()
+    earthquakes = list()
+
+    # We iterate over all items and extract the values we are interested in.
+    for item in data["features"]:
+
+        magnitude = item["properties"]["mag"]
+        place = item["properties"]["place"]
+        url = item["properties"]["url"]
+        time = datetime.fromtimestamp(item["properties"]["time"] / 1000)
+        formatted_time = f"{time:%m-%d-%Y %H:%M:%S}"
+        lon = item["geometry"]["coordinates"][0]
+        lat = item["geometry"]["coordinates"][1]
+
+        earthquakes.append(
+            [magnitude, place, url, formatted_time, lon, lat])
+
+    # We sort the earthquakes by their magnitude (first column).
+    earthquakes.sort(reverse=True)
+
+    # We initiate our Markdown text.
+    markdown = "![Map](./map.png)\n\n# Top 20 Largest Earthquakes in the Past 24 Hours\n\n| Location | Mag | Date and Time (UTC) |\n|:---|:---|:---|\n"
+
+    # These lists will be used to feed the scatter map.
+    longitudes = list()
+    latitudes = list()
+    magnitudes = list()
+
+    # We only take the top 20 records using list indexing.
+    for item in earthquakes[:20]:
+        markdown += f"| [{item[1]}]({item[2]}) | {item[0]} | {item[3]} |\n"
+        longitudes.append(item[4])
+        latitudes.append(item[5])
+
+        # The markers in the map will be the same size as the magnitude plus 5.
+        magnitudes.append(item[0] + 5)
+
+    # Save Markdown to file.
+    open("./README.md", "w", encoding="utf-8").write(markdown)
+
+    generate_map(longitudes, latitudes, magnitudes)
+
+
+def get_data():
+    """Requests the earthquake data from the USGS API.
+
+    Returns
+    -------
+    dict
+        The serialized API data.
+
+    """
+
     # We request the data from the API.
     with requests.get(URL) as response:
 
-        data = response.json()
         print("Data fetched.")
+        return response.json()
 
-        earthquakes = list()
 
-        # We iterate over all items and extract the values we are interested in.
-        for item in data["features"]:
+def generate_map(longitudes, latitudes, magnitudes):
+    """Generates a world map with the given longitudes, latitudes and magnitudes.
+    The 3 lists must have the same length.
 
-            magnitude = item["properties"]["mag"]
-            place = item["properties"]["place"]
-            url = item["properties"]["url"]
-            time = datetime.fromtimestamp(item["properties"]["time"] / 1000)
-            formatted_time = f"{time:%m-%d-%Y %H:%M:%S}"
-            lon = item["geometry"]["coordinates"][0]
-            lat = item["geometry"]["coordinates"][1]
+    Parameters
+    ----------
+    longitudes : list
+        A list of longitudes.
 
-            earthquakes.append(
-                [magnitude, place, url, formatted_time, lon, lat])
+    latitudes : list
+        A list of latitudes.
 
-        # We sort the earthquakes by their magnitude (first column).
-        earthquakes.sort(reverse=True)
+    magnitudes : list
+        A list of magnitudes.
 
-        # We initiate our Markdown text.
-        markdown = "![Map](./map.png)\n\n# Top 20 Largest Earthquakes in the Past 24 Hours\n\n| Location | Mag | Date and Time (UTC) |\n|:---|:---|:---|\n"
+    """
 
-        # These lists will be used to feed the scatter map.
-        longitudes = list()
-        latitudes = list()
+    fig = go.Figure()
 
-        # We only take the top 20 records using list indexing.
-        for item in earthquakes[:20]:
-            markdown += f"| [{item[1]}]({item[2]}) | {item[0]} | {item[3]} |\n"
-            longitudes.append(item[4])
-            latitudes.append(item[5])
+    # We create the scatter map, it is really simple to do as most
+    # of the parameters are just visual customizations.
+    fig.add_traces(go.Scattergeo(lon=longitudes, lat=latitudes,
+                                 mode="markers", marker_color="gold", marker_size=magnitudes))
 
-        # Save Markdown to file.
-        open("./README.md", "w", encoding="utf-8").write(markdown)
+    fig.update_geos(showocean=True, oceancolor="#263238", showlakes=False,
+                    showcountries=True, countrycolor="#FFFFFF", countrywidth=0.5,
+                    framecolor="#FFFFFF", framewidth=1.5, coastlinewidth=0, landcolor="#1B2327")
 
-        fig = go.Figure()
+    # Add final customizations.
+    fig.update_layout(
+        font_color="#FFFFFF",
+        margin={"r": 20, "t": 20, "l": 20, "b": 0},
+        width=1200,
+        height=770,
+        paper_bgcolor="#232b2b",
+        annotations=[
+            dict(
+                x=0.5,
+                y=-0.0,
+                yanchor="bottom",
+                text=f"Source: USGS API | Last updated: {datetime.now():%m-%d-%Y @ %H:%M:%S} (UTC)",
+                font_size=26
+            ),
+            dict(
+                x=0.5,
+                y=0.91,
+                text="Top 20 Largest Earthquakes in the Past 24 Hours",
+                font_size=32
+            )
+        ]
+    )
 
-        # We create the scatter map, it is really simple to do as most
-        # of the parameters are just visual customizations.
-        fig.add_traces(go.Scattergeo(lon=longitudes, lat=latitudes,
-                                     mode="markers", marker_color="gold", marker_size=10))
-
-        fig.update_geos(showocean=True, oceancolor="#263238", showlakes=False,
-                        showcountries=True, countrycolor="#FFFFFF", countrywidth=0.5,
-                        framecolor="#FFFFFF", framewidth=1.5, coastlinewidth=0, landcolor="#1B2327")
-
-        # Add final customizations.
-        fig.update_layout(
-            font_color="#FFFFFF",
-            margin={"r": 20, "t": 20, "l": 20, "b": 0},
-            width=1200,
-            height=770,
-            paper_bgcolor="#232b2b",
-            annotations=[
-                dict(
-                    x=0.5,
-                    y=-0.0,
-                    yanchor="bottom",
-                    text=f"Source: USGS API | Last updated: {datetime.now():%m-%d-%Y @ %H:%M:%S}",
-                    font_size=26
-                ),
-                dict(
-                    x=0.5,
-                    y=0.91,
-                    text="Top 20 Largest Earthquakes in the Past 24 Hours",
-                    font_size=32
-                )
-            ]
-        )
-
-        fig.write_image("./map.png")
-        print("Map created.")
+    fig.write_image("./map.png")
+    print("Map created.")
 
 
 if __name__ == "__main__":
